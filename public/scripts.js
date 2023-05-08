86069e0de19eabfcf18fd7e0581179a6df72f7d6
Cam Burhans <cburhans@email.unc.edu>
2023-05-08 10:04:24 -0400

    fix scripts

--- public/scripts.js
+++ public/scripts.js
@@ -1,148 +1,86 @@
-// If you would like to see some examples of similar code to make an interface interact with an API, 
-// check out the coin-server example from a previous COMP 426 semester.
-// https://github.com/jdmar3/coinserver
+import { rps, rpsls } from './rpsls.js';
 
-function homeNav() {
-    document.getElementById("help").className = "inactive";
-    document.getElementById("home").className = "active";
-}
+// Add an event listener for the DOMContentLoaded event
+document.addEventListener('DOMContentLoaded', () => {
+  // Add event listeners for the game-type and opponent-type select elements
+  document.getElementById('game-type').addEventListener('change', updateMoveOptions);
+  document.getElementById('opponent-type').addEventListener('change', showMoveSelection);
 
-function helpNav() {
-    reset();
-    document.getElementById("home").className = "inactive";
-    document.getElementById("disp").className = "inactive";
-    document.getElementById("help").className = "active";
-}
+  // Add event listener for the play button
+  document.getElementById('play-button').addEventListener('click', playGame);
+});
 
-function reset() {
-    document.getElementById("help").className = "inactive";
-    document.getElementById("home").className = "active";
-    document.getElementById("opponent").className = "inactive";
-    document.getElementById("disp2").className = "inactive";
-    document.getElementById("opponent-options-rps").className = "inactive";
-    document.getElementById("opponent-options-rpsls").className = "inactive";
-    var ele = document.getElementsByName("option");
-    for(var i=0;i<ele.length;i++)
-        ele[i].checked = false;
-    var ele = document.getElementsByName("opp");
-        for(var i=0;i<ele.length;i++)
-            ele[i].checked = false;
-    var ele = document.getElementsByName("player");
-        for(var i=0;i<ele.length;i++)
-            ele[i].checked = false;
-    var ele2 = document.getElementsByName("player2");
-        for(var i=0;i<ele2.length;i++)
-            ele2[i].checked = false;
-    document.getElementById("disp2").innerHTML
-            = "";
-    document.getElementById("disp").innerHTML
-            = "";
-    document.getElementById("result2").className = "inactive"
-    document.getElementById("result").className = "inactive"
-    document.getElementById("result3").className = "inactive"
-    document.getElementById("result4").className = "inactive"
-    document.getElementById("result5").className = "inactive"
-    document.getElementById("result6").className = "inactive"
-}
+function showMoveSelection() {
+    const gameType = document.getElementById('game-type').value;
+    const opponentType = document.getElementById('opponent-type').value;
+    const playButton = document.getElementById('play-button');
 
-function display() {
-    document.getElementById("disp").className = "active";
-    if (document.getElementById("rps").checked) {
-        document.getElementById("disp").innerHTML = "Would you like a random draw or to play an opponent?"
-        document.getElementById("opponent").className = "active";
-        document.getElementById("disp2").className = "active";
-        document.getElementById("game").innerHTML = "rps";
-    } else if (document.getElementById("rpsls").checked) {
-        document.getElementById("disp").innerHTML = "Would you like a random draw or to play an opponent?"
-        document.getElementById("opponent").className = "active";
-        document.getElementById("disp2").className = "active";
-        document.getElementById("game").innerHTML = "rpsls";
-    } else {
-        document.getElementById("disp").innerHTML
-                    = "You have not selected which game you would like to play.";
-    }
-}
-
-function getGame() {
-    if (document.getElementById("opps").checked) {
-        document.getElementById("disp2").innerHTML = "Please select what you would like to play below:";
-        if (document.getElementById("game").innerHTML == "rps") {
-            document.getElementById("opponent-options-rpsls").className = "inactive";
-            document.getElementById("opponent-options-rps").className = "active";
-        } else {
-            document.getElementById("opponent-options-rps").className = "inactive";
-            document.getElementById("opponent-options-rpsls").className = "active";
+    if (gameType && opponentType) {
+        playButton.disabled = false;
+        if(opponentType == "with") {
+            document.querySelector('.move-choice').style.display = 'block';
         }
-    } else if (document.getElementById("no-opponent").checked) {
-        document.getElementById("opponent-options-rps").className = "inactive";
-        document.getElementById("opponent-options-rpsls").className = "inactive";
-        document.getElementById("disp2").innerHTML = "You have selected to play a random opponent.";
-        playRandom();
     } else {
-        document.getElementById("disp2").innerHTML = "You have not selected an opponent style.";
-}
-}
-
-async function playRandom() {
-    document.getElementById("result").className = "active";
-
-    let game = document.getElementById("game").innerHTML
-    let baseurl = window.location.href + 'app/';
-    let url = baseurl + game + '/';
-
-    let response = await fetch(url);
-    let result = await response.json();
-    console.log(result)
-
-    document.getElementById("result2").className = "active"
-    result = result['player']
-    document.getElementById("result2").innerHTML = "<img src='/img/" +result+ ".jpg'>";
-    
-
-}
-
-function getOpp() {
-    if (document.getElementById("rock").checked) {
-        document.getElementById("shot").innerHTML = "rock";
-        playOpp();
-    } else if (document.getElementById("paper").checked) {
-        document.getElementById("shot").innerHTML = "paper";
-        playOpp();
-    } else if (document.getElementById("scissors").checked) {
-        document.getElementById("shot").innerHTML = "scissors";
-        playOpp();
-    } else if (document.getElementById("lizard").checked) {
-        document.getElementById("shot").innerHTML = "lizard";
-        playOpp();
-    } else {
-        document.getElementById("shot").innerHTML = "spock";
-        playOpp();
+        document.querySelector('.move-choice').style.display = 'none';
+        playButton.disabled = true;
     }
-    
-}   
-
-async function playOpp() {
-    document.getElementById("result").className = "active";
-
-    let game = document.getElementById("game").innerHTML
-    let opp = document.getElementById("shot").innerHTML
-    let baseurl = window.location.href + 'app/';
-    let url = baseurl + game + '/play/' + opp + '/';
-
-    let response = await fetch(url);
-    let result = await response.json();
-    console.log(result)
-
-    document.getElementById("result2").className = "active"
-    document.getElementById("result5").className = "active"
-    document.getElementById("result5").innerHTML = result['result'].toUpperCase();
-    document.getElementById("result6").className = "active"
-    player = result['player']
-    document.getElementById("result2").innerHTML = "<img src='/img/" +player+ ".jpg'>";
-    document.getElementById("result3").className = "active"
-    document.getElementById("result4").className = "active"
-    opponent = result['opponent']
-    document.getElementById("result4").innerHTML = "<img src='/img/" +opponent+ ".jpg'>";
-    
-
 }
+
+function updateMoveOptions() {
+    const gameType = document.getElementById('game-type').value;
+    const moveSelect = document.getElementById('player-move');
+    let moveOptions = [];
+
+    if (gameType === 'rps') {
+        moveOptions = ['rock', 'paper', 'scissors'];
+    } else if (gameType === 'rpsls') {
+        moveOptions = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+    }
+
+    moveSelect.innerHTML = '';
+    for (const move of moveOptions) {
+        const option = document.createElement('option');
+        option.value = move;
+        option.textContent = move.charAt(0).toUpperCase() + move.slice(1);
+        moveSelect.appendChild(option);
+    }
+
+    // Call showMoveSelection() to update the Play button state
+    showMoveSelection();
+}
+
+async function sendPlayCallWith(gameType, playerChoice) {
+    try {
+      const response = await fetch(`/app/play/${gameType}/${playerChoice}`);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Error:', error);
+    }
+}
+
+async function sendPlayCallWithout(gameType) {
+  try {
+    const response = await fetch(`/app/play/${gameType}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+  
+async function playGame() {
+    console.log("game played")
+    const opponent = document.getElementById('opponent-type').value;
+    const game = document.getElementById('game-type').value;
+    let gameResult;
+    if(opponent == "with") {
+        const playerChoice = document.getElementById('player-move').value;
+        gameResult = await sendPlayCallWith(game, playerChoice);
+    } else {
+        gameResult = await sendPlayCallWithout(game);
+    }
+
+    // Save gameResult and opponentType to localStorage
+    localStorage.setItem('gameResult', JSON.stringify(gameResult));
+  }
